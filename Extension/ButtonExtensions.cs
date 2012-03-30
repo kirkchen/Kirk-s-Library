34a1776206b7d35185c7d13d35557fec6571adc3
kirkchen <rwk0119@yahoo.com.tw>
2012-03-30 12:39:35 +0800

    add ButtonExtensions.cs

---
+++ Extension/ButtonExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+/// <summary>
+/// Button 擴充功能
+/// </summary>
+public static class ButtonExtensions
+{
+    /// <summary>
+    /// Prevents the double click.
+    /// </summary>
+    /// <param name="button">The button.</param>
+	public static void PreventDoubleClick(this Button button)
+    {
+        var script = @"if(typeof(Page_ClientValidate) == 'function')
+                       {
+                           if(Page_ClientValidate())
+                           {
+                               this.disabled = true;                                                               
+                           }
+                           else
+                           { 
+                               return false;
+                           }
+                       }
+                       else
+                       {
+                           this.disabled = true;                                                           
+                       }";
+       
+        script = script.Replace(Environment.NewLine, string.Empty);       
+
+        button.UseSubmitBehavior = false;
+        button.OnClientClick = script;
+    }
+}
